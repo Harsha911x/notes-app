a9aa732aff2f717f10f9a6d14acf066bbd223900
Harsha911x <149395410+Harsha911x@users.noreply.github.com>
2024-03-23 13:40:22 +0530

    Update supabaseClient.js

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://kbvexiicmzjxtlfbzawt.supabase.co"
-const supabaseAnonKey  = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtidmV4aWljbXpqeHRsZmJ6YXd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY1OTUwOTEsImV4cCI6MjAyMjE3MTA5MX0.zx01Uy5-hpKmQoYPZUYNwYLYDmWAlTO9M-yO-b6T07c"
+const supabaseUrl = "https://cuveebmobvnjfsombuar.supabase.co"
+const supabaseAnonKey  = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1dmVlYm1vYnZuamZzb21idWFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTExODExMjMsImV4cCI6MjAyNjc1NzEyM30.1REw3p7EhUFvDqwcABKt-0BB0AWCG-pNyTALUh8Z7hs"
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export default supabase
